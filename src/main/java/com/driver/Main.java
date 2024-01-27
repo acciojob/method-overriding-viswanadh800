3fb2fe331ea077bcd96686ec06b94d7741f42536
viswanadh800 <viswanadh.800@gmail.com>
2024-01-27 10:51:49 +0530

    Done as said in Readme

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+        B obj = new B();
+        System.out.println(obj.meth());
+        System.out.println(obj.meth());
+    }
 }
