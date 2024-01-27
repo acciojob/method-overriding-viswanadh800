7c927661584453514990180952d66befd7e5e8ba
viswanadh800 <viswanadh.800@gmail.com>
2024-01-27 11:24:43 +0530

    Shifted class A and B inside of Main class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,17 +1,19 @@
 package com.driver;
 
-class A{
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-    @Override
-    public String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-}
+
 public class Main {
+
+     static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+     static class B extends A{
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
     public static void main(String[] args) {
 
         B obj = new B();
