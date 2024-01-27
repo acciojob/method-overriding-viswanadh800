ec85f492c79e649169166a22d7382e440f21bfd1
viswanadh800 <viswanadh.800@gmail.com>
2024-01-27 11:29:09 +0530

    Made class A and B as public static from static

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,12 +3,12 @@ package com.driver;
 
 public class Main {
 
-     static class A{
+     public static class A{
         public String meth(){
             return "Invoking method from class A";
         }
     }
-     static class B extends A{
+     public static class B extends A{
         @Override
         public String meth(){
             return "Method is overridden in Extendend class B";
