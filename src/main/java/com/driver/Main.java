e8b430fd2c3b40cec3a225ca3ca4c2239a4c75d5
viswanadh800 <viswanadh.800@gmail.com>
2024-01-27 11:03:53 +0530

    Removed A.java and B.java files

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B {
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
+}
 public class Main {
     public static void main(String[] args) {
 
